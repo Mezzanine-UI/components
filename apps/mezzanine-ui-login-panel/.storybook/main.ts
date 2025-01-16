2d369ae2bb1382a693566168a56942688193d307
TingTing <ting@rytass.com>
2025-01-16 13:06:45 +0800

    feat: show soruce code

--- apps/mezzanine-ui-login-panel/.storybook/main.ts
+++ apps/mezzanine-ui-login-panel/.storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
     '@storybook/addon-interactions',
+    '@storybook/addon-storysource',
   ],
   framework: {
     name: '@storybook/react-vite',
