03da6a7bf20c2846444647490cb3f11e0e1504fe
TingTing <ting@rytass.com>
2025-01-20 18:56:28 +0800

    feat: login-panel new version

--- apps/mezzanine-ui-login-panel/src/LoginForm/index.tsx
+++ apps/mezzanine-ui-login-panel/src/LoginForm/index.tsx
@@ -10,11 +10,30 @@ import {
 } from './typing';
 
 interface LoginFormProps {
+  /**
+   * 放置 logo
+   */
   logo: ReactNode;
+  /**
+   * 密碼至少需要的長度
+   */
   passwordLength?: number;
+  /**
+   * 密碼不可與前 `number` 代重複
+   */
   generationLimit?: number;
+  /**
+   * `number` 天後需要變更密碼
+   */
   keepPasswordDaysLimit?: number;
+  /**
+   * 登入顯示的字，預設 `登入`
+   */
   generalLoginText?: string;
+  /**
+   * 登入時觸發。
+   * 若需要變更密碼，呼叫 `needChangePassword` 並給予模式，模式有 `FIRST` 和 `TOO_LONG`，對應`初次啟用`和`過久需要變更`
+   */
   onLogin: ({
     values,
     needChangePassword,
@@ -22,6 +41,9 @@ interface LoginFormProps {
     values: LoginFormValues;
     needChangePassword: ({ mode }: { mode: NeedChangePasswordMode }) => void;
   }) => Promise<void>;
+  /**
+   * 變更密碼時觸發
+   */
   onNeedChangePassword: ({
     values,
     account,
@@ -31,6 +53,9 @@ interface LoginFormProps {
     account: string;
     oldPassword: string;
   }) => Promise<boolean>;
+  /**
+   * 寄忘記密碼信時觸發
+   */
   onSendForgetAccount: ({
     values,
   }: {
