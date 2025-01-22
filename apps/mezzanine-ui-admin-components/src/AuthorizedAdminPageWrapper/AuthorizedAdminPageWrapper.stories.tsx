f9d41276441ae851028e99b32d2c294a8173fd35
TingTing <ting@rytass.com>
2025-01-22 15:47:54 +0800

    feat(admin-components): AuthorizedAdminPageWrapper storybook

--- apps/mezzanine-ui-admin-components/src/AuthorizedAdminPageWrapper/AuthorizedAdminPageWrapper.stories.tsx
+++ apps/mezzanine-ui-admin-components/src/AuthorizedAdminPageWrapper/AuthorizedAdminPageWrapper.stories.tsx
@@ -1,5 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { NavigationSubMenu, NavigationItem } from '@mezzanine-ui/react';
+import {
+  NavigationSubMenu,
+  NavigationItem,
+  Button,
+  ModalHeader,
+  ModalBody,
+} from '@mezzanine-ui/react';
 import {
   FormFieldsWrapper,
   InputField,
@@ -7,6 +13,8 @@ import {
 import { ChangePasswordFormValues } from '@mezzanine-ui/login-panel';
 import { useForm } from 'react-hook-form';
 import { useLayout } from '../layout/useLayout';
+import { useDialog } from '../dialog/useDialog';
+import { useModal } from '../modal/useModal';
 import { action } from '@storybook/addon-actions';
 import { PageWrapper } from '../PageWrapper';
 import { AuthorizedAdminPageWrapper } from './index';
@@ -138,6 +146,79 @@ export const FormPage: Story = {
         <PageWrapper title="頁面標題" isFormPage>
           <FormSample />
         </PageWrapper>
+      </AuthorizedAdminPageWrapper>
+    );
+  },
+};
+
+const DialogModalSample = () => {
+  const { openDialog } = useDialog();
+  const { openModal } = useModal();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        gap: 16,
+      }}
+    >
+      <Button
+        type="button"
+        size="large"
+        variant="outlined"
+        onClick={async () => {
+          const isConfirm = await openDialog({
+            severity: 'error',
+            title: '確認刪除？',
+            children: '內容將一併刪除，此動作無法復原。',
+            cancelText: '取消',
+            cancelButtonProps: {
+              danger: false,
+            },
+            confirmText: '刪除',
+          });
+
+          if (isConfirm) {
+            action('onConfirm')();
+          }
+        }}
+      >
+        open dialog
+      </Button>
+      <Button
+        type="button"
+        size="large"
+        variant="outlined"
+        onClick={() => {
+          openModal({
+            width: 500,
+            disableCloseOnBackdropClick: false,
+            hideCloseIcon: false,
+            children: (
+              <>
+                <ModalHeader>Modal Header</ModalHeader>
+                <ModalBody>Modal Body</ModalBody>
+              </>
+            ),
+          });
+        }}
+      >
+        open modal
+      </Button>
+    </div>
+  );
+};
+export const WithDialogAndModal: Story = {
+  args,
+  parameters: {
+    controls: { include: ['headerHeight', 'sidebarWidth', 'role', 'account'] },
+  },
+  render: function Render(args) {
+    return (
+      <AuthorizedAdminPageWrapper {...args}>
+        <DialogModalSample />
       </AuthorizedAdminPageWrapper>
     );
   },
