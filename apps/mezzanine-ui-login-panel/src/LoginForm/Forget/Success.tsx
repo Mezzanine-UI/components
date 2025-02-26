b333f3b008d8cb668aad725e5347a4932e4731dd
TingTing <ting@rytass.com>
2025-02-26 15:58:32 +0800

    feat(login-panel): send forget password email success view

--- apps/mezzanine-ui-login-panel/src/LoginForm/Forget/Success.tsx
+++ apps/mezzanine-ui-login-panel/src/LoginForm/Forget/Success.tsx
@@ -11,40 +11,32 @@ interface SuccessProps {
 const Success: FC<SuccessProps> = ({ account, onBack }) => {
   return (
     <div className={cx(classes.root, classes.success)}>
-      <div className={cx(classes.logoWrapper, classes.success)}>
-        <Icon icon={CheckCircleFilledIcon} color="success" size={50} />
-      </div>
-      <div className={classes.formWrapper}>
-        <Typography variant="h2" color="text-primary">
-          密碼重置連結已發送至
-        </Typography>
-        <Typography variant="h5" color="text-primary">
-          {account}
-        </Typography>
-        <Typography
-          variant="h5"
-          color="text-primary"
-          component="p"
-          align="center"
-        >
-          請於1小時內點擊URL設定密碼。
-          <br />
-          若未收到email或遇URL失效請聯絡管理員。
-        </Typography>
-        <div className={classes.blocksWrapper}>
-          <div className={classes.blockWrapper}>
-            <Button
-              type="button"
-              variant="contained"
-              size="large"
-              className={classes.button}
-              onClick={onBack}
-            >
-              返回登入頁面
-            </Button>
-          </div>
-        </div>
-      </div>
+      <Icon icon={CheckCircleFilledIcon} color="success" size={50} />
+      <Typography variant="h2" color="text-primary">
+        密碼重置連結已發送至
+      </Typography>
+      <Typography variant="h5" color="text-primary">
+        {account}
+      </Typography>
+      <Typography
+        variant="caption"
+        color="text-secondary"
+        component="p"
+        align="center"
+      >
+        請於1小時內點擊連結設定密碼。
+        <br />
+        若未收到email或遇連結失效請聯絡管理員。
+      </Typography>
+      <Button
+        type="button"
+        variant="contained"
+        size="large"
+        className={classes.backButton}
+        onClick={onBack}
+      >
+        返回登入頁面
+      </Button>
     </div>
   );
 };
