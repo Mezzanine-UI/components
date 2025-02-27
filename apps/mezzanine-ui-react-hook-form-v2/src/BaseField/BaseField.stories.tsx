0c2fe32128e840a6235457851090e549a2881680
TingTing <ting@rytass.com>
2025-02-27 15:46:30 +0800

    feat(react-hook-form-v2): basefield storybook

---
+++ apps/mezzanine-ui-react-hook-form-v2/src/BaseField/BaseField.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from '@mezzanine-ui/react';
+import { useForm } from 'react-hook-form';
+import { FormFieldsWrapper } from '../FormFieldsWrapper';
+import { BaseField } from './index';
+
+const meta = {
+  component: BaseField,
+} satisfies Meta<typeof BaseField>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+interface DemoFormValues {
+  inputName: string;
+}
+
+export const Default: Story = {
+  args: {
+    name: 'inputName',
+    label: '姓名',
+    disabled: false,
+    required: true,
+    children: (
+      <Input fullWidth size="large" placeholder="請輸入姓名" clearable />
+    ),
+    remark: 'remark',
+    hints: [
+      {
+        severity: 'warning',
+        text: 'warning',
+      },
+      {
+        severity: 'info',
+        text: 'info',
+      },
+      {
+        severity: 'error',
+        text: 'error',
+      },
+      {
+        severity: 'success',
+        text: 'success',
+      },
+    ],
+  },
+  parameters: {
+    controls: {
+      include: ['disabled', 'required'],
+    },
+  },
+  render: function Render(args) {
+    const methods = useForm<DemoFormValues>({
+      defaultValues: {
+        inputName: '',
+      },
+    });
+
+    return (
+      <FormFieldsWrapper methods={methods}>
+        <BaseField {...args} />
+      </FormFieldsWrapper>
+    );
+  },
+};
