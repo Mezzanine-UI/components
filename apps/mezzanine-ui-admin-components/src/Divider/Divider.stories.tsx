a2cb9165ea901cc35ca8044187d0dc5b90b0fd29
TingTing <ting@rytass.com>
2025-03-13 09:51:31 +0800

    feat(admin-components): more divider color

--- apps/mezzanine-ui-admin-components/src/Divider/Divider.stories.tsx
+++ apps/mezzanine-ui-admin-components/src/Divider/Divider.stories.tsx
@@ -11,9 +11,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    color: 'divider',
+  },
   parameters: {
-    controls: { include: [] },
+    controls: { include: ['color'] },
   },
   render: function Render(args) {
     return (
--- apps/mezzanine-ui-admin-components/src/Divider/index.tsx
+++ apps/mezzanine-ui-admin-components/src/Divider/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, CSSProperties } from 'react';
 import { cx } from '@mezzanine-ui/react';
 import classes from './index.module.scss';
 
@@ -7,14 +7,33 @@ export interface DividerProps {
    * 如果為 true，則沒有上下 margin
    */
   isPure?: boolean;
+  /**
+   * divider 顏色
+   */
+  color?:
+    | 'divider'
+    | 'border'
+    | 'primary'
+    | 'secondary'
+    | 'warning'
+    | 'success'
+    | 'error';
 }
 
 /**
  * 分隔線
  */
-export const Divider: FC<DividerProps> = ({ isPure = false }) => {
+export const Divider: FC<DividerProps> = ({
+  isPure = false,
+  color = 'divider',
+}) => {
+  const colorStyleVar = {
+    '--divider-color': `var(--mzn-color-${color})`,
+  } as CSSProperties;
+
   return (
     <div
+      style={colorStyleVar}
       className={cx(classes.host, {
         [classes.isPure]: isPure,
       })}
